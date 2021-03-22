0afc5df8f188d27e78fbb2bade17c72ea1cedca2
Abhishek Kamalkishor <32305263+Abhi41288@users.noreply.github.com>
2021-03-23 00:20:05 +0530

    lets chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
+import React from 'react'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="40a252cd-7886-402a-862a-a792f24cbc80"
+            userName="shek"
+            userSecret="abhi1988"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        />
+    )
 }
 
-export default App;
+export default App
